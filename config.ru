5cb081fd2db35b36fb27d926234fb8868a705e1e
Nemo <nemo@razorpay.com>
2018-07-20 19:46:24 +0530

    [metrics] Export limited metrics
    - Expose a single bank_lookups metrics that counts successful lookups
      against each bank code

--- config.ru
+++ config.ru
@@ -3,7 +3,5 @@ require 'prometheus/middleware/collector'
 require 'prometheus/middleware/exporter'
 
 use Rack::Deflater
-use Prometheus::Middleware::Collector
-use Prometheus::Middleware::Exporter
 
 run Sinatra::Application
---
+++ metrics.rb
@@ -0,0 +1,17 @@
+require 'prometheus/client'
+require 'prometheus/client/formats/text'
+
+class Metrics
+  def initialize
+    @bank_metrics = Prometheus::Client.registry.counter(:bank_lookups, "A counter of successful lookups made against each bank")
+  end
+
+  def increment(ifsc)
+    code = ifsc[0...4]
+    @bank_metrics.increment(bank: code)
+  end
+
+  def format
+    Prometheus::Client::Formats::Text.marshal(Prometheus::Client.registry)
+  end
+end
